c5e45d73f8d6081b606c15ddb2bed9c2182f4b8f
Michael Tarleton <michael.tarleton@me.com>
2021-07-10 00:37:57 -0500

    Added styleing to the search box

--- content-script.js
+++ content-script.js
@@ -176,12 +176,32 @@ const addSearchField = function (playlistDivSelector, playlistsSelector) {
     const originalChildNodes = [].slice.call(childNodes, 0)
 
     // Create the search box and attach to top of playlist
+
+    // Div
+    const searchBar3 = document.createElement('div')
+    searchBar3.className =
+        'input-content label-is-floating style-scope tp-yt-paper-input-container'
+    searchBar3.style.paddingLeft = '2rem'
+    searchBar3.style.paddingRight = '-2rem'
+    searchBar3.style.paddingTop = '1rem'
+
+    // Iron Input
+    const searchBar2 = document.createElement('iron-input')
+    searchBar2.className = 'input-element style-scope tp-yt-paper-input'
+    searchBar2.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>'
+    searchBar2.getElementsByTagName('svg')[0].style.float = 'left'
+    searchBar2.getElementsByTagName('svg')[0].style.position = 'relative'
+
+    // Search Bar
     const searchBar = document.createElement('input')
     searchBar.placeholder = 'Search...'
+    searchBar.className = 'style-scope tp-yt-paper-input'
+    searchBar.style.minWidth = '80%'
+    searchBar.style.width = "auto"
+    searchBar.style.display = "inline-block"
 
-    // TODO: Style the search box
-    searchBar.classList.add('style-scope')
-    searchBar.classList.add('tp-yt-paper-input')
+    // Style the search box
 
     // Create keyup event to trigger search
     const search = (event) => {
@@ -204,7 +224,9 @@ const addSearchField = function (playlistDivSelector, playlistsSelector) {
     searchBar.addEventListener('keyup', search)
 
     // Insert it before the playlist
-    playlistsNode.parentNode.insertBefore(searchBar, playlistsNode)
+    searchBar3.appendChild(searchBar2)
+    searchBar2.appendChild(searchBar)
+    playlistsNode.parentNode.insertBefore(searchBar3, playlistsNode)
 }
 
 /**
